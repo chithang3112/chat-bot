c1660720ba6fe215ceec6ca1d8eac3969e222fef
chithang3112 <chithang@pro-seeds.co.jp>
2018-09-05 17:13:23 +0900

    asd

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const LINE_CHANNEL_ACCESS_TOKEN = 'lTKkZ0/fYh1QKcRf3OVPVCI303Dbff8ijAETN3Qw105r1
 
 app.post('/webhook',function(req, res){
      res.status(200).end();
-     console.log(req.body);
+     console.log(req);
 });
 
 httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
