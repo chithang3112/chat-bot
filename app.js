170f1d6291c6e06156929197ab5abcf4dec4a241
chithang3112 <chithang@pro-seeds.co.jp>
2018-09-05 16:52:35 +0900

    custom

--- app.js
+++ app.js
@@ -16,29 +16,6 @@ const LINE_CHANNEL_ACCESS_TOKEN = 'lTKkZ0/fYh1QKcRf3OVPVCI303Dbff8ijAETN3Qw105r1
 
 app.post('/webhook',function(req, res){
      res.status(200).end();
-    for (var event of req.body.events){
-        if (event.type == 'message' && event.message.text == 'ハロー'){
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
-            }
-            var body = {
-                replyToken: event.replyToken,
-                messages: [{
-                    type: 'text',
-                    text: 'こんにちは'
-                }]
-            }
-            var url = 'https://api.line.me/v2/bot/message/reply';
-            request({
-                url: url,
-                method: 'POST',
-                headers: headers,
-                body: body,
-                json: true
-            });
-        }
-    }
 });
 
 httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
