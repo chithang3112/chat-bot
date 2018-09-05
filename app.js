1f8d5ecd7a500904820ed2d9958e2dbf60e5fc27
chithang3112 <chithang@pro-seeds.co.jp>
2018-09-05 16:30:38 +0900

    asd

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ let express = require('express');
 
 let http = require('http');
 
-var app = express(function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-});
-
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
 
 var httpServer = http.createServer(app);
 
@@ -14,9 +12,33 @@ var io = require('socket.io')(httpServer);
 
 let PORT = process.env.PORT || 80;
 
-app.get('/webhook',function(req, res){
-    // res.send('aaa');
-    res.sendStatus(200);
+const LINE_CHANNEL_ACCESS_TOKEN = 'lTKkZ0/fYh1QKcRf3OVPVCI303Dbff8ijAETN3Qw105r1buKUJf1mRdL2R/7mPlVpK9qTl5fNjrzV7Hvra/opiFFpo9koce/5Y6GvVMZaClXmZ2Xna38SLzQOi0Yj9AdreuBU00UsYTLm9I2aV0H1wdB04t89/1O/w1cDnyilFU=';
+
+app.post('/webhook',function(req, res){
+     res.status(200).end();
+    for (var event of req.body.events){
+        if (event.type == 'message' && event.message.text == 'ハロー'){
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+            }
+            var body = {
+                replyToken: event.replyToken,
+                messages: [{
+                    type: 'text',
+                    text: 'こんにちは'
+                }]
+            }
+            var url = 'https://api.line.me/v2/bot/message/reply';
+            request({
+                url: url,
+                method: 'POST',
+                headers: headers,
+                body: body,
+                json: true
+            });
+        }
+    }
 });
 
 httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
