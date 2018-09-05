07aa802eb500f87838d5c9dc5f0f3d0fd0fb7eca
chithang3112 <chithang@pro-seeds.co.jp>
2018-09-05 16:01:23 +0900

    asd

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var io = require('socket.io')(httpServer);
 let PORT = process.env.PORT || 80;
 
 app.get('/webhook',function(req, res){
-    res.send('aaa');
+    // res.send('aaa');
+    res.sendStatus(200);
 });
 
 httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
