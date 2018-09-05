3c065a02eb59f6072c13c9268cf94efae6650789
chithang3112 <chithang@pro-seeds.co.jp>
2018-09-05 15:45:26 +0900

    asd

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+
+let http = require('http');
+
+var app = express();
+
+var httpServer = http.createServer(app);
+
+var io = require('socket.io')(httpServer);
+
+app.get('/webhook',function(req, res){
+    res.send('aaa');
+});
